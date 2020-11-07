1597f528c9fe883e9844172e3cbe6a90f68e8bf3
Johnny Casey <jjcasey@users.noreply.github.com>
2020-11-07 14:39:39 -0600

    Explicitly mount Cuda 10.0 and libcudnn7

--- start.sh
+++ start.sh
@@ -9,6 +9,8 @@ docker run -td \
 	--tmpfs /run/lock:size=100M \
 	-v /sys/fs/cgroup:/sys/fs/cgroup:ro \
 	-v ${PWD}/tensorflow-conf.json:/opt/shinobi/plugins/tensorflow/conf.json \
+	-v /usr/local/cuda-10.0:/usr/local/cuda-10.0 \
+	-v /usr/lib/aarch64-linux-gnu/libcudnn.so.7:/usr/lib/aarch64-linux-gnu/libcudnn.so.7 \
 	-p 8082:8082 \
 	--network lan-services \
 	--runtime nvidia \
